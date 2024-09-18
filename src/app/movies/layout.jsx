719aa3db130106376417e806148c90a6b4f7843b
genesisgap <genesispatrick@gmail.com>
2024-09-18 10:45:40 +0100

    Feat: Modification on MovieShow and Updating Changes New Week3

--- src/app/movies/layout.jsx
+++ src/app/movies/layout.jsx
@@ -4,6 +4,7 @@ const sat = Roboto({ subsets: ["latin"] , weight: ['400']});
 
 export default function MovieLayout({children}) {
   return (
+    
     <div className={sat.className} >
         {children}
     </div>
